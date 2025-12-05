ff6b11f3d4c29e0daa1921ee62d49319b040a097
amiljoseph03 <amiljoseph03@gmail.com>
2025-12-05 14:47:21 +0530

    ui updated

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,41 +1,4 @@
-// 'use client';
 
-// import { useCart } from '@/app/context/CartContext';
-// import Link from 'next/link';
-
-// export default function CartPage() {
-//   const { cart } = useCart();
-
-//   if (cart.length === 0) {
-//     return (
-//       <div style={{ textAlign: 'center', marginTop: '40px' }}>
-//         <h2>Your cart is empty</h2>
-//         <Link href="/">Go back to Home</Link>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div>
-//       <h1>Your Cart</h1>
-
-//       {cart.map((item, index) => (
-//         <div
-//           key={index}
-//           style={{
-//             border: '1px solid #ddd',
-//             padding: '10px',
-//             marginBottom: '10px',
-//           }}
-//         >
-//           <h3>{item.name}</h3>
-//           <p>₹{item.price}</p>
-//           <img src={item.image} width={150} />
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
 
 
 'use client';
