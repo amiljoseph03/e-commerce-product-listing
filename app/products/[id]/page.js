f7fe57fc604b6ead2f95b596aacba5ad23dc5d13
amiljoseph03 <amiljoseph03@gmail.com>
2025-12-05 13:39:56 +0530

    product is added to cart

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,49 +1,72 @@
+// 'use client';
+// import { use } from 'react';
+// import { products } from '@/data/products';
+// import Link from 'next/link';
+// // import '.../styles/producttt.css';
+// import '@/app/styles/producttt.css';
+
+
+// function getParams(paramsPromise) {
+//   return use(paramsPromise); 
+// }
+
+// export default function ProductDetails({ params}) {
+//   const resolvedParams = getParams(params); 
+//   const productId = parseInt(resolvedParams.id, 10);
+//   const product = products.find((p) => p.id === productId);
+
+//   if (!product) return <h2>Product not found</h2>;
+
+//   return (
+//     <div className='one'>
+//       <Link href="/"> Back to Home</Link>
+//       <h1>{product.name}</h1>
+//       <p>₹{product.price}</p>
+//       <p>{product.description}</p>
+//       <img src={product.image} alt={product.name} width={400} />
+//       <button>Add to Cart</button>
+//     </div>
+//   );
+// }
+
+
+
 'use client';
+
 import { use } from 'react';
 import { products } from '@/data/products';
+import { useCart } from '@/app/context/CartContext';
 import Link from 'next/link';
-// import '.../styles/producttt.css';
 import '@/app/styles/producttt.css';
 
-
 function getParams(paramsPromise) {
-  return use(paramsPromise); 
+  return use(paramsPromise);
 }
 
-export default function ProductDetails({ params}) {
-  const resolvedParams = getParams(params); 
+export default function ProductDetails({ params }) {
+  const resolvedParams = getParams(params);
   const productId = parseInt(resolvedParams.id, 10);
+
+  const { addToCart } = useCart();
+
   const product = products.find((p) => p.id === productId);
 
   if (!product) return <h2>Product not found</h2>;
 
   return (
-    <div className='one'>
-      <Link href="/"> Back to Home</Link>
+    <div className="one">
+      <Link href="/">Back to Home</Link>
+
       <h1>{product.name}</h1>
       <p>₹{product.price}</p>
       <p>{product.description}</p>
       <img src={product.image} alt={product.name} width={400} />
-      <button>Add to Cart</button>
+
+      <button onClick={() => addToCart(product)}>Add to Cart</button>
+
+      <Link href="/cart">
+        <button>View Cart</button>
+      </Link>
     </div>
   );
 }
-
-
-// export default async function ProductDetails({ params }) {
-//   const res = await fetch(`http://localhost:3000/api/products/${params.id}`, {
-//     cache: 'no-store',
-//   });
-//   const product = await res.json();
-
-//   if (!product.id) return <h2>Product not found</h2>;
-
-//   return (
-//     <div>
-//       <h1>{product.name}</h1>
-//       <p>₹{product.price}</p>
-//       <p>{product.description}</p>
-//       <img src={product.image} alt={product.name} width={400} />
-//     </div>
-//   );
-// }
