a65ed4e94e6deb18600bb09623cab2d307a57cc3
amiljoseph03 <amiljoseph03@gmail.com>
2025-12-05 11:00:23 +0530

    created home page with  products images

---
+++ data/products.js
@@ -0,0 +1,27 @@
+
+export const products = [
+  {
+    id: 1,
+    name: 'Deer light',
+    price: 300,
+    description:
+      'Elegant deer hand table lamp – a beautifully crafted decorative light featuring a delicate deer holding a glowing orb. Perfect for adding a warm, cozy ambiance to any room or as a unique accent piece for your home décor.',
+    image: '/p1.jpg',
+  },
+  {
+    id: 2,
+    name: 'Fox light',
+    price: 300,
+    description:
+      'Elegant deer hand table lamp – a beautifully crafted decorative light featuring a delicate deer holding a glowing orb. Perfect for adding a warm, cozy ambiance to any room or as a unique accent piece for your home décor.',
+    image: '/p2.jpg',
+  },
+  {
+    id: 3,
+    name: 'lighnig kitten',
+    price: 300,
+    description:
+      'Elegant deer hand table lamp – a beautifully crafted decorative light featuring a delicate deer holding a glowing orb. Perfect for adding a warm, cozy ambiance to any room or as a unique accent piece for your home décor.',
+    image: '/p3.jpg',
+  },
+];
